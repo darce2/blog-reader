409115b98ff826b0967887c2c5c764b862b4146a
Arlen D'Arcy <Arlen D'Arcy>
2016-01-26 23:23:59 -0500

    First commit

---
+++ app/src/main/java/com/newagehacker/blogreader/MyActivity.java
@@ -0,0 +1,80 @@
+package com.newagehacker.blogreader;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+    //the key to the intents extra. Public is used so that the next activity can query it.
+    public final static String EXTRA_MESSAGE = "com.newagehacker.blogreader.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+   //Called whenever the user hits the send button
+    public void sendMessage(View view){
+        //do something in response to button press
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //This starts the new activity and passes it the intent
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                //noinspection SimplifiableIfStatement
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+//            case R.id.search_bar:
+//                //User chose the search button start
+//                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
